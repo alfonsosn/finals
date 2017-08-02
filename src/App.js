c93f560a05f611718d870db05119b1a4e0529388
Alfonso Enriquez Castro <alfonso.en.ca@gmail.com>
2017-08-02 12:03:02 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -121,15 +121,18 @@ function Classes(props){
                 obj.examSched = classe["examSched"]
                 return obj
               })
-  
+
   return(
+    <div>
+    <h3> Possible exam results</h3>
     <DataTable
         shadow={0}
         rows={rowsForTable}>
         <TableHeader name="classHours" tooltip="When Your Class Meets">Class Hours</TableHeader>
         <TableHeader name="examDate" tooltip="Date of Your Final">Exam Date</TableHeader>
-        <TableHeader name="examSched" tooltip="Hours of your Final">Price</TableHeader>
+        <TableHeader name="examSched" tooltip="Hours of your Final">Exam Schedule</TableHeader>
     </DataTable>
+    </div>
   )
 }
 
