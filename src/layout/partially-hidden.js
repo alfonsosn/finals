8ed89df50f52f4c4cca6760c8573b48476f228b9
Alfonso Enriquez Castro <alfonso.en.ca@gmail.com>
2017-08-17 17:14:59 -0400

    blur for firefox

--- src/layout/partially-hidden.js
+++ src/layout/partially-hidden.js
@@ -4,7 +4,7 @@ import {Card} from 'react-mdl';
 
 const PartiallyHidden = (props) => {
     return(
-      <Card shadow={0} style={{filter: "blur(10px)", width: 'auto', margin: '10px 0px'}}>
+      <Card shadow={0} style={{filter: "blur(10px)", width: 'auto', margin: '10px 0px', filter: 'blur(10px)' }}>
        <h3>{props.title}</h3>
       </Card>
     )
